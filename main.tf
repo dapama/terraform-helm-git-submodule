40f8ec140a3b975b653b5312fc2d30e0823c86ac
David Pacheco <dpachecomarroyo@gmail.com>
2022-11-10 14:24:52 +0100

    Update main.tf, git_clone.sh, and variables.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+
+module "helm_release" {
+  source = "git@github.com:terraform-module/terraform-helm-release.git?ref=v2.8.0"
+
+  namespace         = var.namespace
+  app               = var.app
+  repository        = var.repository
+  repository_config = var.repository_config
+  values            = var.values
+  set               = var.set
+  set_sensitive     = var.set_sensitive
+}
+
+resource "null_resource" "git_clone" {
+  provisioner "local-exec" {
+    command     = "./scripts/git_clone.sh -t 300"
+    environment = {
+      REPOSITORY_NAME = var.repository_name
+      TAG             = var.tag
+    }
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  depends_on = [
+    "module.helm_release"
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "namespace" {
+  description = "namespace where to deploy an application"
+  type        = string
+}
+
+variable "app" {
+  description = "an application to deploy"
+  type        = map(any)
+}
+
+variable "repository_config" {
+  description = "repository configuration"
+  type        = map(any)
+  default     = {}
+}
+
+variable "values" {
+  description = "Extra values"
+  type        = list(string)
+  default     = []
+}
+
+variable "set" {
+  description = "Value block with custom STRING values to be merged with the values yaml."
+  type = list(object({
+    name  = string
+    value = string
+  }))
+  default = null
+}
+
+variable "set_sensitive" {
+  description = "Value block with custom sensitive values to be merged with the values yaml that won't be exposed in the plan's diff."
+  type = list(object({
+    path  = string
+    value = string
+  }))
+  default = null
+}
+
+variable "repository" {
+  description = "Helm repository"
+  type        = string
+}
+
+variable "repository_name" {
+  type        = string
+  description = "Name of the repository to retrieve the release from."
+}
+
+variable "tag" {
+  type        = string
+  description = "Repository tag."
+}
