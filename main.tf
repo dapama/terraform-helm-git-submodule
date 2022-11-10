d36695fc80b2949362a5ace8d691488e5cbde94a
David Pacheco <dpachecomarroyo@gmail.com>
2022-11-10 18:51:32 +0100

    Update main.tf and git_clone.sh

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
 
-module "helm_release" {
+resource "null_resource" "git_submodule_init" {
+  provisioner "local-exec" {
+    command     = "git submodule add https://github.com/argoproj/argo-helm --branch $TAG"
+    environment = {
+      TAG = lookup(var.app, "version", "latest")
+    }
+    interpreter = ["bash", "-c"]
+  }
+  
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
+module "helm_release_argocd" {
   source = "git@github.com:terraform-module/terraform-helm-release.git?ref=v2.8.0"
 
   namespace         = var.namespace
@@ -9,19 +24,14 @@ module "helm_release" {
   values            = var.values
   set               = var.set
   set_sensitive     = var.set_sensitive
-}
-
-resource "null_resource" "git_submodule_init" {
-  provisioner "local-exec" {
-    command     = "cd ../../.. && git submodule update --init"
-    interpreter = ["bash", "-c"]
-  }
-
-  triggers = {
-    always_run = "${timestamp()}"
-  }
 
   depends_on = [
-    module.helm_release
+    resource.null_resource.git_submodule_init
   ]
 }
+
+# module "helm_release_argocd_apps" {
+#   depends_on = [
+#     module.helm_release_argocd
+#   ]
+# }
--- scripts/git_clone.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pwd
-# cd $PATH
-# git submodule update --init
