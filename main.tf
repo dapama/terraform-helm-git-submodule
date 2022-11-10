fb824e3c264db90461f64328e44637dca272dc96
David Pacheco <dpachecomarroyo@gmail.com>
2022-11-10 14:36:16 +0100

    test delete module

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 
-module "helm_release" {
-  source = "git@github.com:terraform-module/terraform-helm-release.git?ref=v2.8.0"
+# module "helm_release" {
+#   source = "git@github.com:terraform-module/terraform-helm-release.git?ref=v2.8.0"
 
-  namespace         = var.namespace
-  app               = var.app
-  repository        = var.repository
-  repository_config = var.repository_config
-  values            = var.values
-  set               = var.set
-  set_sensitive     = var.set_sensitive
-}
+#   namespace         = var.namespace
+#   app               = var.app
+#   repository        = var.repository
+#   repository_config = var.repository_config
+#   values            = var.values
+#   set               = var.set
+#   set_sensitive     = var.set_sensitive
+# }
 
 resource "null_resource" "git_clone" {
   provisioner "local-exec" {
